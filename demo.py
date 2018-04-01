5442d6565d7697a0e253bd45568523e85c922e82
Kunal <kunal1998doon@gmail.com>
2018-04-01 22:35:04 +0530

    new open search and about completed

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
 #NOT FINAL CODE 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QMainWindow, QAction, QFileSystemModel, QTreeView, QVBoxLayout 
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QMainWindow, QAction, QFileSystemModel, QTreeView, QVBoxLayout, QMessageBox, QInputDialog ,QFileDialog
 from PyQt5.QtGui import QIcon, QFont
+import webbrowser
 import os
+import win32api
 
 """view class allows the creation of the path struture of the disk"""
 class view(QWidget):
@@ -13,11 +15,12 @@ class view(QWidget):
     def initUI(self):
         self.setGeometry(0,0,800,600)
         self.model = QFileSystemModel()
+        self.model.setReadOnly(False)
         self.model.setRootPath("")
         self.tree = QTreeView()
         self.tree.setModel(self.model)
         self.tree.setAnimated(True)
-        self.tree.setIndentation(20)
+        self.tree.setIndentation(10)
         self.tree.setSortingEnabled(True)
         self.tree.setWindowTitle("Dir View")
         self.tree.resize(640, 480)
@@ -26,7 +29,6 @@ class view(QWidget):
         self.setLayout(windowLayout)
         self.show()
         
-
 """" creates the main window for displaying the various elements"""
 class demo(QMainWindow):
     
@@ -40,20 +42,10 @@ class demo(QMainWindow):
         
         self.setGeometry(300, 300, 800, 600)
         self.setWindowTitle('File_Manager')
-        self.model=QFileSystemModel()
         dirn=os.path.dirname(__file__)
         dirn1=dirn+"/Icon"
         os.chdir(dirn+"/ test")
         print(os.getcwd())
-        self.model.setRootPath("")
-        self.tree=QTreeView()
-        self.tree.setModel(self.model)
-        self.tree.setAnimated(False)
-        self.tree.setIndentation(20)
-        self.tree.setSortingEnabled(True)
-        windowsLayout=QVBoxLayout()
-        windowsLayout.addWidget(self.tree)
-        self.setLayout(windowsLayout)
         self.setWindowIcon(QIcon(dirn1+'/files_ico32.png'))   
         menu=self.menuBar()
         menu.setNativeMenuBar(False)
@@ -63,10 +55,17 @@ class demo(QMainWindow):
         searchA=QAction(QIcon(dirn1+"/search_ico24.png"),"Search",self)
         searchA.setStatusTip("search for a file")
         searchA.setShortcut("Ctrl+Q")
+        searchA.triggered.connect(self.search)
         searchM.addAction(searchA)
         helpM=menu.addMenu("Help")
         newA=QAction(QIcon(dirn1+"/new_ico24.png"),"New",self)
         newA.setShortcut("Ctrl+N")
+        newA.triggered.connect(self.newFile)
+        openA=QAction(QIcon(dirn1+"/open_ico24.png"),"Open",self)
+        openA.setShortcut("Ctrl+O")
+        openA.setStatusTip("Opens a file in its native application")
+        openA.triggered.connect(self.openFile)
+        fileM.addAction(openA)
         renameA=QAction(QIcon(dirn1+"/rename_ico24.png"),"rename",self)
         renameA.setShortcut("Ctrl+R")
         renameA.setStatusTip("Renames a file")
@@ -84,15 +83,46 @@ class demo(QMainWindow):
         fileM.addAction(exitA)
         aboutA=QAction(QIcon(dirn1+"/about_ico24.png"),"About",self)
         aboutA.setStatusTip("Displays Information about the applicatin")
+        aboutA.triggered.connect(self.openurl)
         helpM.addAction(aboutA)
         self.setCentralWidget(view())
         self.show()
         #self.commands()
         
+    def openurl(self):
+       webbrowser.open("https://github.com/velociraptor98/File_Manager")
+    
+    def search(self):
+        a=False
+        fileN,ok=QInputDialog.getText(self,"File search","enter the name of the file")
+        if ok:
+            dirN,okp=QInputDialog.getText(self,"File Search","enter the directory you want to search");
+            if okp:
+                dirN=dirN+"\\"
+                for root, dirs, files in os.walk(dirN):
+                    self.statusBar().showMessage(os.path.join(root, fileN))
+                    if fileN in files:
+                        fileFound=win32api.GetShortPathName((os.path.join(root,fileN)))
+                        print(fileFound)
+                        QMessageBox.about(self,"Result",fileFound)
+                        a=True
+                        break
+                if(a==False):
+                    QMessageBox.about(self,"Result","No such file found")
+                    
+    def newFile(self):
+     options=QFileDialog.Options()
+     fileName, _ = QFileDialog.getSaveFileName(self,"New","","All Files (*);;Text Files (*.txt);;Python(*.py);;C++(*.cpp);;Java(*.java)", options=options)
+     if fileName:
+         f=open(fileName,"w+")
+         f.close()
+    
+    def openFile(self):
+        options=QFileDialog.Options()
+        fileName,_=QFileDialog.getOpenFileName(self,"Open","","All Files (*)",options=options)
+        if(fileName):
+            os.startfile(fileName)
         
-    """def commands(self):
-        a=input("enter the name of the new file")+".txt"
-        file=open(a,"a")"""
         
 if __name__ == '__main__':
     
